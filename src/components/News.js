c5faeb383341c9eb9ac401f7b03d135b61be5d37
AhmedAsim-69 <ahmedk082@gmail.com>
2023-07-09 21:42:31 +0500

    Designed Template for news

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,20 @@ import NewsItem from './NewsItem'
 export default class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component
-        <NewsItem/>
+      <div className='container my-3'>
+        <h2>News - Top Headlines</h2>
+        <div className="container"></div>
+        <div className="row">
+            <div className="col-md-4">
+                <NewsItem title="My Title" description="myDesc"/>
+            </div>
+            <div className="col-md-4">
+                <NewsItem title="My Title" description="myDesc"/>
+            </div>
+            <div className="col-md-4">
+                <NewsItem title="My Title" description="myDesc"/>
+            </div>
+        </div>
       </div>
     )
   }
